89bb4b0572c0751e993c48e1fad00b163ab490cf
hadmarine <contact@hadmarine.com>
2022-10-09 23:26:46 +0200

    WIP

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: process.env.NODE_ENV === 'production' ? '.env' : '.env.dev',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.DB_HOST, {
+      auth: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+      },
+      dbName: process.env.DB_NAME,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
