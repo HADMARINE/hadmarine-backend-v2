d8be73e234e2cc3d1b5908444c41bc93229aae3d
hadmarine <contact@hadmarine.com>
2022-10-26 22:53:15 +0200

    Fix dep issue of JwtAuthGuard

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -2,31 +2,47 @@ import { ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 import { Observable } from 'rxjs';
+import { AuthorizationFailedException } from 'src/errors/exceptions/authorization-failed.exception';
 import { AuthorityEnum } from 'src/users/authority.enum';
 import { UserDocument } from 'src/users/user.schema';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
-  constructor(private reflector: Reflector, ...args: any[]) {
-    super(reflector, ...args);
-  }
-
-  canActivate(
+  handleRequest<TUser = any>(
+    err: any,
+    user: any,
+    info: any,
     context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    if (!super.canActivate(context)) {
-      return false;
+    status?: any,
+  ): TUser {
+    if (err || !user) {
+      throw new AuthorizationFailedException();
     }
-
-    const roles = this.reflector.get<AuthorityEnum>(
-      'roles',
-      context.getHandler(),
-    );
-    if (!roles) {
-      return true;
-    }
-
-    const request = context.switchToHttp().getRequest();
-    const user: UserDocument = request.user;
+    return user;
   }
 }
+
+// {
+//   constructor(private reflector: Reflector, ...args: any[]) {
+//     super(reflector, ...args);
+//   }
+
+//   canActivate(
+//     context: ExecutionContext,
+//   ): boolean | Promise<boolean> | Observable<boolean> {
+//     if (!super.canActivate(context)) {
+//       return false;
+//     }
+
+//     const roles = this.reflector.get<AuthorityEnum>(
+//       'roles',
+//       context.getHandler(),
+//     );
+//     if (!roles) {
+//       return true;
+//     }
+
+//     const request = context.switchToHttp().getRequest();
+//     const user: UserDocument = request.user;
+//   }
+// }
