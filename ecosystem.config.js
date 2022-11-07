f2a7da320125b60354fe97cbe1b816b260351060
hadmarine <contact@hadmarine.com>
2022-11-07 21:22:54 +0100

    fix: pm2 script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const packageData = require('./package.json');
 
 const apps = [
@@ -5,7 +6,7 @@ const apps = [
     name: packageData.name,
     exec_mode: 'cluster',
     instances: 'max',
-    script: './dist/index.js',
+    script: './dist/src/main.js',
     watch: false,
     env: {
       NODE_ENV: 'production',
